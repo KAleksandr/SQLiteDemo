3b76bca55488e058d4e0310d60a782c9c10ffffc
KAleksandr <okuzmenkoa@gmail.com>
2023-08-21 18:03:30 +0300

    Creating a database access class

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteDemo.Repositories
+{
+    public class CustomerRepository
+    {
+        SQLiteConnection connection;
+        public CustomerRepository()
+        {
+            connection = new SQLiteConnection(Сonstans.DatabasePath, Сonstans.Flags);
+        }
+    }
+}
