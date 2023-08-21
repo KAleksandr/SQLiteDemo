5e00ee81ade260f22cc826af9d8488a6d5d6095d
KAleksandr <okuzmenkoa@gmail.com>
2023-08-21 18:39:32 +0300

    Updating a customer

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -4,6 +4,7 @@ using SQLiteDemo.MVVM.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,20 +18,73 @@ namespace SQLiteDemo.Repositories
         {
             connection = new SQLiteConnection(Сonstans.DatabasePath, Сonstans.Flags);
             connection.CreateTable<Customer>();
-            
+
         }
-         public void Add(Customer newCustomer)
+        public void Add(Customer newCustomer)
         {
             int result = 0;
             try
             {
-              result = connection.Insert(newCustomer);
+                result = connection.Insert(newCustomer);
                 StatusMessage = $"{result} row(s) added";
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 StatusMessage = $"Error {ex.Message}";
             }
+        }
+        public List<Customer> GetAll()
+        {
+            try
+            {
+                return connection.Table<Customer>().ToList();
+            }
+            catch (Exception ex)
+            { StatusMessage = $"Error {ex.Message}"; }
+            return null;
+        }
+        public List<Customer> GetAll2()
+        {
+            try
+            {
+                return connection.Query<Customer>("select * from Customers").ToList();
+            }
+            catch (Exception ex)
+            { StatusMessage = $"Error {ex.Message}"; }
+            return null;
+        }
+        public Customer Get(int id)
+        {
+            try {
+            return connection.Table<Customer>().FirstOrDefault(x => x.Id == id);
+            }
+            catch(Exception ex) 
+            {
+                StatusMessage = $"Error {ex.Message}";
+            }
+            return null;
+        }
+        public void AddOrUpdate(Customer customer)
+        {
+            int result = 0;
+            try
+            {
+                if(customer.Id == 0)
+                {
+                    result = connection.Insert(customer);
+                }
+                else
+                {
+                    result = connection.Update(customer);
+                }
+               
+                StatusMessage = $"{result} row(s) updated";
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error {ex.Message}";
+            }
+           
         }
     }
 }
